20b34a7e32be22e32ffcdbf2d5ecf700f3ea99bd
Davi Rolim <davi.delima@hotmail.com>
2018-05-30 20:57:32 -0300

    Adição de metodos à classe Banco

--- src/banco_package/Banco.java
+++ src/banco_package/Banco.java
@@ -13,6 +13,25 @@ public class Banco {
 	public void insereCliente(Cliente cliente) {
 		listaClientes.add(cliente);
 	}
-	
+	public void insereConta(Cliente cliente) {
+		Conta c = new Conta(cliente);
+		this.listaContas.add(c);
+	}
+	//TODO excluir cliente
+	public void excluiConta(int numeroConta) {
+		for(Conta c : this.listaContas) {
+			if(c.getNumConta() == numeroConta) {
+				this.listaContas.remove(c);
+				break;
+			}
+		}
+	}
+	public void deposita(int numeroConta, double valor) {
+		for(Conta c: this.listaContas) {
+			if(c.getNumConta() == numeroConta) {
+				c.creditaConta(valor, "Deposito");//TODO testar
+			}
+		}
+	}
 	
 }

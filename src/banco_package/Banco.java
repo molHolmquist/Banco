279d7db7f61e80c54ad68f4ced7f778940446a8e
Davi Rolim <davi.delima@hotmail.com>
2018-05-31 00:27:10 -0300

    metodo excluiCliente com defeitos

--- src/banco_package/Banco.java
+++ src/banco_package/Banco.java
@@ -1,23 +1,48 @@
 package banco_package;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Banco {
 	private String nomeBanco;
-	private ArrayList<Cliente> listaClientes;
-	private ArrayList<Conta> listaContas;
+	private ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
 	
 	Banco(String nomeBanco){
 		this.nomeBanco = nomeBanco;
 	}
 	public void insereCliente(Cliente cliente) {
-		listaClientes.add(cliente);
+		this.listaClientes.add(cliente);
 	}
 	public void insereConta(Cliente cliente) {
 		Conta c = new Conta(cliente);
 		this.listaContas.add(c);
 	}
-	//TODO excluir cliente
+	//TODO excluir cliente	
+	public void excluiCliente(String cpfOuCnpj) {
+		boolean found = false;
+		Iterator<Cliente> itr = this.listaClientes.iterator();
+		while(itr.hasNext()) {
+			Cliente c = itr.next();
+			if(c.getCpf_cnpj().equals(cpfOuCnpj)) {
+				System.out.println("Rodou isso");
+				Iterator<Conta> itrcont = this.listaContas.iterator();
+				while(itrcont.hasNext()) {
+					Conta cont = itrcont.next();
+					if(c.equals(cont.getCliente())) {
+						System.out.println("Cliente possui conta");
+						found = false;
+						break;
+					}
+					else {found = true;}
+				}
+			}
+			if(found) {				
+				itr.remove();
+			}
+		}
+	}
+	
 	public void excluiConta(int numeroConta) {
 		for(Conta c : this.listaContas) {
 			if(c.getNumConta() == numeroConta) {
@@ -29,9 +54,17 @@ public class Banco {
 	public void deposita(int numeroConta, double valor) {
 		for(Conta c: this.listaContas) {
 			if(c.getNumConta() == numeroConta) {
-				c.creditaConta(valor, "Deposito");//TODO testar
+				c.creditaConta(valor, "Deposito");//TODO t
 			}
 		}
+	}
+	
+	
+	public ArrayList<Cliente> getListaCliente(){
+		return this.listaClientes;
+	}
+	public ArrayList<Conta> getListaContas(){
+		return this.listaContas;
 	}
 	
 }
--- src/banco_package/main_teste.java
+++ src/banco_package/main_teste.java
@@ -1,6 +1,7 @@
 package banco_package;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -9,37 +10,48 @@ public class main_teste {
 	public static void main(String[] args) {
 		
 		Cliente juse = new Cliente("Juse","07320176610","Rua Jose Afonso","3241-5264");
-		Conta contaJuse = new Conta(juse);
-		contaJuse.creditaConta(50, "Valor recebido por loucura do juse.");
-		contaJuse.creditaConta(300, "Vendeu a bicicleta.");
-		contaJuse.creditaConta(5000, "Ganhou na loteria.");
-		contaJuse.creditaConta(200, "Achou no chao.");
+//		Conta contaJuse = new Conta(juse);
+//		contaJuse.creditaConta(50, "Valor recebido por loucura do juse.");
+//		contaJuse.creditaConta(300, "Vendeu a bicicleta.");
+//		contaJuse.creditaConta(5000, "Ganhou na loteria.");
+//		contaJuse.creditaConta(200, "Achou no chao.");
 		
 		Cliente arnaldo = new Cliente("Arnaldo","09087987709","Rua Jose Afonso","3241-5264");
-		Conta contaArnaldo = new Conta(arnaldo);
-		contaArnaldo.creditaConta(100,  "Restaurante Boi dos Vales");
-		contaArnaldo.creditaConta(50,  "Restaurante Boi dos Vales");
-		contaArnaldo.creditaConta(20,  "Arroz");
-		contaArnaldo.creditaConta(300,  "Batata");
+//		Conta contaArnaldo = new Conta(arnaldo);
+//		contaArnaldo.creditaConta(100,  "Restaurante Boi dos Vales");
+//		contaArnaldo.creditaConta(50,  "Restaurante Boi dos Vales");
+//		contaArnaldo.creditaConta(20,  "Arroz");
+//		contaArnaldo.creditaConta(300,  "Batata");
 
 		GregorianCalendar calendar = new GregorianCalendar();
 		
+		Banco bradesco = new Banco("bradesco"); 
+		bradesco.insereCliente(juse);
+		//bradesco.insereConta(juse);
+		bradesco.insereCliente(arnaldo);
+		bradesco.insereConta(arnaldo);
+		bradesco.excluiCliente("07320176610");
 		
-		int mYear = calendar.get(Calendar.YEAR);
-		int mMonth = calendar.get(Calendar.MONTH);
-		int mDay = calendar.get(Calendar.DAY_OF_MONTH);
-		
-		System.out.println(mDay + "/" + mMonth + "/" + mYear + " ");
-
-		
-		for(Movimentacao m: contaArnaldo.extrato()) {
-			
-			SimpleDateFormat fmt = new SimpleDateFormat("dd-MMM-yyyy");
-		    fmt.setCalendar(m.getDataMov());
-		    String dateFormatted = fmt.format(m.getDataMov().getTime());
-			System.out.println(m.getDescricao() + "-mes:"+ " " + dateFormatted);
-			
+		ArrayList<Cliente> c = bradesco.getListaCliente();
+		for(Cliente m:c) {
+			System.out.println(m.getNomeCliente());
 		}
+		
+//		int mYear = calendar.get(Calendar.YEAR);
+//		int mMonth = calendar.get(Calendar.MONTH);
+//		int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+//		
+//		System.out.println(mDay + "/" + mMonth + "/" + mYear + " ");
+//
+//		
+//		for(Movimentacao m: contaArnaldo.extrato()) {
+//			
+//			SimpleDateFormat fmt = new SimpleDateFormat("dd-MMM-yyyy");
+//		    fmt.setCalendar(m.getDataMov());
+//		    String dateFormatted = fmt.format(m.getDataMov().getTime());
+//			System.out.println(m.getDescricao() + "-mes:"+ " " + dateFormatted);
+//			
+//		}
 		
 	}
 

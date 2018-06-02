7cb74f729a090a314483d5519ae10992f1723f0b
Davi Rolim <davi.delima@hotmail.com>
2018-06-02 13:57:25 -0300

    Metodo excluiCliente

--- src/banco_package/Interface.java
+++ src/banco_package/Interface.java
@@ -18,4 +18,28 @@ public class Interface {
 		Cliente cliente = new Cliente(nome, c, end, f);
 		this.banco.insereCliente(cliente);
 	}
+	public void excluiCliente() {
+		//TODO testar
+		System.out.println("Os clientes existentes são:");
+		int i = 1;
+		for(Cliente c : this.banco.getListaCliente()) {
+			System.out.println(i+"-"+c.getNomeCliente()+c.getCpf_cnpj());
+			i++;
+		}
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Qual você deseja exlcuir?");
+		int n = scan.nextInt();
+		if(n <= i || n > 0) {
+			i = 1;
+			for(Cliente c : this.banco.getListaCliente()) {
+				if( i == n) {
+					this.banco.excluiCliente(c.getNomeCliente());
+				}
+				i++;
+			}
+		}
+		else
+			System.out.println("Numero invalido");
+		
+	}
 }
